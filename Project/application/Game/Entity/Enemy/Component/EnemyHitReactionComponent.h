d6ed3f985f6e354795fa3aaa1cc51a7cec52dd7c
Nakagawa_Rikuto <146906446+nakagawa-rikuto@users.noreply.github.com>
2026-01-11 15:51:29 +0900

    攻撃を受けた時の点滅を追加。

--- Project/application/Game/Entity/Enemy/Component/EnemyHitReactionComponent.h
+++ Project/application/Game/Entity/Enemy/Component/EnemyHitReactionComponent.h
@@ -14,6 +14,12 @@ private:
 		float slowdownMultiplier = 1.0f; // 減速率の乗数
 		bool isHit = false;				 // ヒットしたかどうか
 	};
+	/// ===色の状態構造体=== ///
+	struct ColorState {
+		float colorTImer = 0.0f;    // 色変化用タイマー
+		float colorInterval = 0.0f; // 色変化の間隔
+		bool isColorChange = false; // 色変化フラグ
+	};
 
 public:
 	/// ===設定パラメータの構造体=== ///
@@ -21,18 +27,23 @@ public:
 		float knockBackForce = 1.5f;	// ノックバックの強さ
 		float slowdownFactor = 0.3f;	// 減速率(0.0~1.0、小さいほど遅くなる)
 		float slowdownDuration = 0.2f;	// 減速効果の持続時間
+		float alphaDuration = 0.2f;		// 透明度変化の持続時間
+		float hitAlpha = 0.6f;			// ヒット時の透明度
+		float flashSpeed = 10.0f;		// 点滅速度
 	};
 
 	/// ===更新用コンテキスト=== ///
 	struct UpdateContext {
 		Vector3 currentPosition;	// 現在の位置
 		Vector3 currentVelocity;	// 現在の速度
+		Vector4 currentColor;    // 現在の色
 		float deltaTime;
 	};
 
 	/// ===更新結果=== ///
 	struct UpdateResult {
 		Vector3 velocity = { 0.0f, 0.0f, 0.0f };
+		Vector4 color = { 1.0f, 1.0f, 1.0f, 1.0f };
 	};
 
 public:
@@ -72,18 +83,20 @@ public: /// ===Getter=== ///
 	// KnockBackConfig
 	const KnockBackConfig& GetConfig() const { return config_; }
 	// KnockBackState
-	const KnockBackState& GetState() const { return state_; }
+	const KnockBackState& GetKnockBackState() const { return state_; }
+	// ColorState
+	const ColorState& GetColorState() const { return colorState_; }
 
 #ifdef USE_IMGUI
 public: /// ===Setter=== ///
 	// KnockBackConfig
 	void ApplyConfig(const KnockBackConfig& newConfig);
 #endif // USE_IMGUI
-
 private:
 	/// ===情報=== ///
 	KnockBackConfig config_;
 	KnockBackState state_;
+	ColorState colorState_;
 
 	// ヒットした相手の方向
 	Vector3 hitParentDirection_ = {0.0f, 0.0f, 0.0f};
@@ -100,5 +113,12 @@ private:
 	/// 減速係数の更新処理
 	/// </summary>
 	void UpdateSlowdownMultiplier();
+
+	/// <summary>
+	/// 渡された色を元に更新された色を返します。`
+	/// </summary>
+	/// <param name="color">更新の基となる色。参照渡しされた const Vector4 オブジェクト。</param>
+	/// <returns>更新された色を表す Vector4 オブジェクト。</returns>
+	Vector4 UpdateColor(const Vector4& color);
 };
 
